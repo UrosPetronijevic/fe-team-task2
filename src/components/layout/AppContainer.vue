24006e57b95465c6ab4cb1a7f646b8131127da51
UrosPetronijevic <ukipetlja@gmail.com>
2026-04-06 12:36:06 -0400

    Fix AppContainer and main.ts

--- src/components/layout/AppContainer.vue
+++ src/components/layout/AppContainer.vue
@@ -1,7 +1,14 @@
-<script setup lang="ts"></script>
+<!-- src/components/layout/AppContainer.vue -->
+<script setup lang="ts">
+import { useAuthStore } from "@/stores/auth";
+import TopBar from "./TopBar.vue";
+
+const authStore = useAuthStore();
+</script>
 
 <template>
   <div class="app-wrapper">
+    <TopBar v-if="authStore.isLoggedIn" />
     <main>
       <RouterView />
     </main>
